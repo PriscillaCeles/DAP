f1a277cc2287ef51f5b35a703dc94bbc07cdae4f
Priscilla Celes Lima <pri.c.maciel@hotmail.com>
2022-04-20 18:06:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const $buttonMenuOpen = document.querySelector('.button-mobile-open')
-const $botaoMenuClose = document.querySelector('.button-mobile-close')
+const $buttonMenuClose = document.querySelector('.button-mobile-close')
+const $main = document.querySelector('main')
+
 const $Menu = document.querySelector('.nav-mobile')
 
 $buttonMenuOpen.addEventListener('click', function () { 
     $Menu.classList.add('nav-mobile--ativo')
     })
 
-$botaoMenuClose.addEventListener('click', function () {
+$buttonMenuClose.addEventListener('click', function () {
+        $Menu.classList.remove('nav-mobile--ativo')
+    })
+
+$main.addEventListener('click', function () {
         $Menu.classList.remove('nav-mobile--ativo')
     })
